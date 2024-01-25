6d110f5de58cccfc90412d00132b12eb6504b421
Octo8080 <okutannderuta0529@gmail.com>
2024-01-25 22:23:56 +0900

    first

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <h1 class="text-4xl font-bold">
-          Babylon.js Physics simulation by 'Havok'.
+          Babylon.js Physics simulation by 'Havok'!
         </h1>
         <BabylonSimulationControl count={count} />
         <div id="babylonMount" width="400" height="400"></div>
